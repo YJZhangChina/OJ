1b30b3b371237be1c12a57fb636477c3e7e80a62
zhangyingjie <zhangyingjie@megvii.com>
2019-03-10 13:16:39 +0800

    v1.0

---
+++ leetcode/java/array/P729MyCalendarI.java
@@ -0,0 +1,23 @@
+package java.array;
+
+import java.util.TreeMap;
+
+public class P729MyCalendarI {
+
+    private TreeMap<Integer, Integer> map;
+
+    public P729MyCalendarI() {
+        map = new TreeMap<Integer, Integer>();
+    }
+
+    public boolean book(int start, int end) {
+        Integer prev = map.floorKey(start), next = map.ceilingKey(start);
+        if ((prev == null || map.get(prev) <= start) && (next == null || end <= next)) {
+            map.put(start, end);
+            return true;
+        }
+        return false;
+    }
+
+}
+
