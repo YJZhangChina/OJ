f4846fab1a415995277a06234b9c9ce1280628e1
YJZhangChina <zhangyingjie.china@gmail.com>
2019-04-06 14:56:12 +0800

    559. Maximum Depth of N-ary Tree

---
+++ leetcode/java/dfs/P559.java
@@ -0,0 +1,22 @@
+package leetcode.java.dfs;
+
+import leecode.java.util.NArrayNode;
+
+public class P559 {
+
+	// Change type NarrayNode to Node
+	public int maxDepth(NArrayNode root) {
+		if (root == null) {
+			return 0;
+		}
+		if (root.children == null || root.children.size() == 0) {
+			return 1;
+		}
+		int ans = 0;
+		for (int i = 0; i < root.children.size(); i++) {
+			ans = Math.max(ans, 1 + maxDepth(root.children.get(i)));
+		}
+		return ans;
+	}
+
+}
