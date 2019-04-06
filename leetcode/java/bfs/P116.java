6a1a384ffda60966e1e8f8c11c911d939e4bd325
YJZhangChina <zhangyingjie.china@gmail.com>
2019-04-06 17:24:39 +0800

    116. Populating Next Right Pointers in Each Node

---
+++ leetcode/java/bfs/P116.java
@@ -0,0 +1,24 @@
+package leetcode.java.bfs;
+
+import leecode.java.util.TreeNodeNext;
+
+public class P116 {
+
+	// Change the type NarrayNode to Node
+	public TreeNodeNext connect(TreeNodeNext root) {
+		if(root != null && root.left != null && root.right != null) {
+			iterating(root.left, root.right);
+		}
+		return root;
+	}
+	
+	// Change the type NarrayNode to Node
+	public void iterating(TreeNodeNext left, TreeNodeNext right) {
+		if(left != null && right != null) {
+			left.next = right;
+			iterating(left.left, left.right);
+			iterating(left.right, right.left);
+			iterating(right.left, right.right);
+		}
+	}
+}
