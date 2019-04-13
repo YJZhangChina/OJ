3fe2de6a7c0cf4f8acc157b5c66a74d42fd3e34a
YJZhangChina <zhangyingjie.china@gmail.com>
2019-04-13 14:33:26 +0800

    129. Sum Root to Leaf Numbers

---
+++ leetcode/java/dfs/P129.java
@@ -0,0 +1,27 @@
+package leetcode.java.dfs;
+
+import leecode.java.util.TreeNode;
+
+public class P129 {
+
+	int ans = 0;
+
+	public int sumNumbers(TreeNode root) {
+		if (root != null) {
+			dfs(root, 0);
+		}
+		return ans;
+	}
+
+	public void dfs(TreeNode root, int current) {
+		if (root.left == null && root.right == null) {
+			ans += (current * 10 + root.val);
+		}
+		if (root.left != null) {
+			dfs(root.left, current * 10 + root.val);
+		}
+		if (root.right != null) {
+			dfs(root.right, current * 10 + root.val);
+		}
+	}
+}
