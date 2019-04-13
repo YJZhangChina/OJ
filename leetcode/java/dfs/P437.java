fa93c2a85a7090287c7b85620bbc274759f43ae0
YJZhangChina <zhangyingjie.china@gmail.com>
2019-04-13 15:47:59 +0800

    437. Path Sum III

---
+++ leetcode/java/dfs/P437.java
@@ -0,0 +1,33 @@
+package leetcode.java.dfs;
+
+import leecode.java.util.TreeNode;
+
+public class P437 {
+
+	int ans = 0;
+
+	public int pathSum(TreeNode root, int sum) {
+		search(root, sum);
+		return ans;
+	}
+
+	public void search(TreeNode root, int sum) {
+		if (root != null) {
+			dfs(root, sum);
+			search(root.left, sum);
+			search(root.right, sum);
+		}
+	}
+
+	public void dfs(TreeNode root, int target) {
+		if (root == null) {
+			return;
+		}
+		if (root.val == target) {
+			ans += 1;
+		}
+		dfs(root.left, target - root.val);
+		dfs(root.right, target - root.val);
+	}
+
+}
