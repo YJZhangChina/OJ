624fecbbbde7ef6ba264c300e2f1db5fa810c47d
YJZhangChina <zhangyingjie.china@gmail.com>
2019-05-03 12:54:36 +0800

    461. Hamming Distance

---
+++ leetcode/java/bit/P461.java
@@ -0,0 +1,21 @@
+package leetcode.java.bit;
+
+public class P461 {
+
+	public static void main(String[] args) {
+		new P461().hammingDistance(1, 4);
+	}
+
+	public int hammingDistance(int x, int y) {
+		int ans = 0;
+		int bit = 1;
+		for (int i = 0, j = 32 - 1; i < j; ++i) {
+			if ((x & bit) != (y & bit)) {
+				ans += 1;
+			}
+			bit <<= 1;
+		}
+		return ans;
+	}
+
+}
