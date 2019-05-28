bd92c69a073ee04f5aa0f59aa7b3af09218d8ce1
YJZhangChina <zhangyingjie.china@gmail.com>
2019-05-28 18:33:24 +0800

    377. Combination Sum IV

--- leetcode/java/dp/P523.java
+++ leetcode/java/dp/P523.java
@@ -5,10 +5,18 @@ import leetcode.java.util.Util;
 public class P523 {
 
     public static void main(String[] args) {
-        System.out.println(new P523().checkSubarraySum(new int[] {23, 2, 6, 4, 7}, 0));
-        System.out.println(new P523().checkSubarraySum(new int[] {1, 0, 1, 0, 1}, 4));
-        System.out.println(new P523().checkSubarraySum(new int[] {0, 1, 0}, 0));
-        System.out.println(new P523().checkSubarraySum(new int[] {0, 0}, -1));
+        P523 object = new P523();
+        int[][] nums = new int[][]{
+                {23, 2, 6, 4, 7}, // false
+                {1, 0, 1, 0, 1},// false
+                {0, 1, 0},// false
+                {0, 0},// true
+        };
+        int[] k = new int[]{0, 4, 0, -1};
+        int length = 4;
+        for (int i = 0; i < length; ++i) {
+            System.out.println(object.checkSubarraySum(nums[i], k[i]));
+        }
     }
 
     public boolean checkSubarraySum(int[] nums, int k) {
@@ -17,11 +25,11 @@ public class P523 {
             return false;
         }
         int[] cache = new int[length + 1];
-        for(int i = 0; i < length; ++i) {
+        for (int i = 0; i < length; ++i) {
             cache[i + 1] = cache[i] + nums[i];
         }
         for (int j = 1; j < length; ++j) {
-            for (int i = 0; i < j;  ++i) {
+            for (int i = 0; i < j; ++i) {
                 int y = j + 1, x = i;
                 int value = cache[y] - cache[x];
                 if (k != 0 && value % k == 0) {
